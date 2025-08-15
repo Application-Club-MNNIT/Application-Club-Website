4674f456b344de3158fdafd8d380395c381004df
JaTin-at-git <jigyashusaini7@gmail.com>
2025-08-15 15:56:32 +0530

    adding ts-node to dev script

--- client/src/AxiosRequests/backendRequestAxios.ts
+++ client/src/AxiosRequests/backendRequestAxios.ts
@@ -4,7 +4,6 @@ const BASE_URL: string = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.withCredentials = true;
 
 export const backend = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true,
-  //   credentials: "include",  // used on fetch iD
+    baseURL: BASE_URL,
+    withCredentials: true,
 });
